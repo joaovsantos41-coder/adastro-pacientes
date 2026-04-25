8e4524d582fb937e378fb58833492c7a669e28bb
Seu Nome <seuemail@gmail.com>
2026-04-24 23:23:12 -0300

    Atualização do projeto

--- script.js
+++ script.js
@@ -3,6 +3,48 @@ const client = window.supabase.createClient(
   "sb_publishable_Wm-I_G_LSc5vHPx3vrf7Ow_FUdidRFZ"
 );
 
+let editandoId = null;
+
+async function carregar() {
+  const { data } = await client
+    .from("pacientes")
+    .select("*")
+    .order("id");
+
+  const lista = document.getElementById("lista");
+  lista.innerHTML = "";
+
+  data.forEach((p) => {
+    lista.innerHTML += `
+      <div class="item">
+        <b>${p.nome}</b><br>
+        ${p.celular}<br>
+        ${p.email}<br><br>
+
+        <div class="acoes">
+          <button class="edit" onclick="editar(${p.id}, '${p.nome}', '${p.celular}', '${p.email}')">Editar</button>
+          <button class="del" onclick="excluir(${p.id})">Excluir</button>
+        </div>
+      </div>
+    `;
+  });
+}
+
+async function excluir(id) {
+  await client.from("pacientes").delete().eq("id", id);
+  carregar();
+}
+
+function editar(id, nome, celular, email) {
+  editandoId = id;
+
+  document.getElementById("nome").value = nome;
+  document.getElementById("celular").value = celular;
+  document.getElementById("email").value = email;
+
+  document.querySelector("button[type=submit]").textContent = "Salvar Alterações";
+}
+
 document.getElementById("form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
@@ -10,15 +52,24 @@ document.getElementById("form").addEventListener("submit", async (e) => {
   const celular = document.getElementById("celular").value;
   const email = document.getElementById("email").value;
 
-  const { error } = await client
-    .from("pacientes")
-    .insert([{ nome, celular, email }]);
+  if (editandoId) {
+    await client
+      .from("pacientes")
+      .update({ nome, celular, email })
+      .eq("id", editandoId);
 
-  if (error) {
-    alert("Erro ao cadastrar!");
-    console.log(error);
+    editandoId = null;
+    document.querySelector("button[type=submit]").textContent = "Cadastrar";
   } else {
+    await client
+      .from("pacientes")
+      .insert([{ nome, celular, email }]);
+
     document.getElementById("mensagem").style.display = "block";
-    document.getElementById("form").reset();
   }
+
+  document.getElementById("form").reset();
+  carregar();
 });
+
+carregar();
